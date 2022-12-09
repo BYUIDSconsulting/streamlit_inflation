b3b970243376ac155857db7d76a27fcd2fd3084f
allisonbday <allisonbday@byui.edu>
2022-12-09 14:23:37 -0700

    updated title

--- pages/4_📉_more_graphs.py
+++ pages/4_📉_more_graphs.py
@@ -12,7 +12,7 @@ st.image(
     "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/chart-decreasing_1f4c9.png",
     width=100,
 )
-st.title("Machine Learning Model Explanation")
+st.title("More Graphs")
 st.caption("By Asa Carlson and Camilo Hozman")
 path = os.path.join(os.path.dirname(__file__), "..")
 
