131b8a4209f675e00d0a4d2db1e6140cc8ddb907
allisonbday <allisonbday@byui.edu>
2022-12-09 14:15:13 -0700

    just copied working app

--- 📈_state_inflation.py
+++ 📈_state_inflation.py
@@ -1,6 +1,7 @@
 # IMPORTS ---------------------------------------------------------------------
 import pandas as pd
 import yaml
+import os
 
 # classes
 from fred_resources.fredSubclasses import *
@@ -45,7 +46,7 @@ columns = dataMap["columns"]
 # FUNCTIONS -------------------------------------------------------------------
 @st.cache
 def read_unintermaster():
-    master = pd.read_csv(r"final_datasets\uninterpolated_master.csv")
+    master = pd.read_csv(r"final_datasets/uninterpolated_master.csv")
     master = master.drop(["Unnamed: 0"], axis=1)
     return master
 
@@ -107,29 +108,33 @@ st.image(
     width=100,
 )
 st.title("United States Construction Inflation")
-
+path = os.path.dirname(__file__)
 # INITIALIZE CLASSES ----------------------------------------------------------
 Bussiness_Applications = BusinessApplications(
-    r"fred_resources\fred_yamls\BusinessApplications.yml"
+    path + "/fred_resources/fred_yamls/BusinessApplications.yml"
 )
 Construction_Employees = ConstructionEmployees(
-    r"fred_resources\fred_yamls\ConstructionEmployees.yml"
+    path + "/fred_resources/fred_yamls/ConstructionEmployees.yml"
 )
 Construction_Wages = ConstructionWages(
-    r"fred_resources\fred_yamls\\ConstructionWages.yml"
+    path + "/fred_resources/fred_yamls/ConstructionWages.yml"
 )
 HighPropriety_Businesses = HPBusinessApplications(
-    r"fred_resources\fred_yamls\\HPBusinessApplications.yml"
+    path + "/fred_resources/fred_yamls/HPBusinessApplications.yml"
 )
-House_Price_Index = HousePriceIdx(r"fred_resources\fred_yamls\\HousePriceIdx.yml")
-New_Housing = NewHousingPermits(r"fred_resources\fred_yamls\\NewHousingPermits.yml")
-Real_GDP = RealGDP(r"fred_resources\fred_yamls\\RealGDP.yml")
+House_Price_Index = HousePriceIdx(path + "/fred_resources/fred_yamls/HousePriceIdx.yml")
+New_Housing = NewHousingPermits(
+    path + "/fred_resources/fred_yamls/NewHousingPermits.yml"
+)
+Real_GDP = RealGDP(path + "/fred_resources/fred_yamls/RealGDP.yml")
 HighPropriety_NAICs = TotalHighPropensityNAICs(
-    r"fred_resources\fred_yamls\\TotalHighPropensityNAICs.yml"
+    path + "/fred_resources/fred_yamls/TotalHighPropensityNAICs.yml"
 )
-NAICs = TotalNAICs(r"fred_resources\fred_yamls\\TotalNAICs.yml")
-Unemployment_Rate = Unemployment(r"fred_resources\fred_yamls\\unemployment.yml")
-Zillow_Home_Value = ZillowHomeValue(r"fred_resources\fred_yamls\\ZillowHomeValue.yml")
+NAICs = TotalNAICs(path + "/fred_resources/fred_yamls/TotalNAICs.yml")
+Unemployment_Rate = Unemployment(path + "/fred_resources/fred_yamls/unemployment.yml")
+Zillow_Home_Value = ZillowHomeValue(
+    path + "/fred_resources/fred_yamls/ZillowHomeValue.yml"
+)
 
 # load data
 uninterMaster = read_unintermaster()
@@ -178,7 +183,7 @@ st.subheader("ML Model")
 
 
 def load_model(months):
-    model_path = r"models\XGBoost_{0}.pkl".format(months)
+    model_path = "XGBoost_{0}.pkl".format(months)
     model = pickle.load(open(model_path, "rb"))
     return model
 
@@ -294,7 +299,7 @@ st.subheader("Sources Pulled")
 
 @st.cache
 def master_codes():
-    df = pd.read_csv(r"final_datasets\master_codes.csv")
+    df = pd.read_csv(path + "/final_datasets/master_codes.csv")
     # df = df.drop(["Unnamed: 0"], axis=1)
     df = df.groupby("Name")[
         "frequency", "units", "seasonal_adjustment", "title", "id"
